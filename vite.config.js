6bc878b0f168d4b16f2b30aa0ca2792b3ad59ebc
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2024-05-05 16:34:21 +0200

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://feduk01.github.io/Webb-Shop/',
   plugins: [react()],
+  base: './'
 })
